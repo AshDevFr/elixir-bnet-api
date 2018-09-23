64e44419fa7669091b37ce3f956dcd22400b230d
Sylvain CAU <ashdevfr@gmail.com>
2018-09-23 14:44:05 -0700

    Error helper

--- lib/battle_net/utils.ex
+++ lib/battle_net/utils.ex
@@ -3,13 +3,20 @@ defmodule BattleNet.Utils do
   """
 
   @doc"""
-
   """
-
   def ensure_locale(region, locale \\ nil) do
     case locale do
       nil -> BattleNet.Locales.code(BattleNet.Regions.default_locale(region))
       l -> l.code
     end
+  end
+
+  @doc"""
+  """
+  def err_to_string(%{code: code, detail: detail, type: type}) do
+    "#{code}(#{type}) - #{detail}"
+  end
+  def err_to_string(_) do
+    ""
   end
 end
